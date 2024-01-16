e5f3e5d3ac2bb9b5f210ff6a1d81fdbc546f2f20
sukant.kumar <sukant.kumar@ril.com>
2024-01-16 15:02:11 +0530

    Added bootstrap and added card class to article title, date and description

--- data/ArticleList.js
+++ data/ArticleList.js
@@ -1,8 +1,20 @@
 const Article_List = [
-    { title: 'Article1'},
-    {title: 'Article2'},
-    {title: 'Article3'},
-    {title: 'Article4'},
+    { title: 'Article1',
+    description:'This is going to be an article about MLOps',
+    date: '2023-12-24',
+    },
+    {title: 'Article2',
+    description:'This is going to be an article about FrontEnd',
+    date:'2023-12-30',
+    },
+    {title: 'Article3',
+    description:'This is going to be an article about Self-Help',
+    date:'2024-01-05',
+    },
+    {title: 'Article4',
+    description:'This is going to be an article about Public Speaking',
+    date: '2024-01-25',
+    },
 ];
 
 module.exports = Article_List;
