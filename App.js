2830df7de9236de367134be38004f58d4fd42096
sukant.kumar <sukant.kumar@ril.com>
2024-01-16 10:13:51 +0530

    Added ejs as view enegine and created basic index.ejs page inside views folder to be rendered

--- App.js
+++ App.js
@@ -2,8 +2,11 @@ const express = require('express');
 const app = express();
 const PORT = 5024;
 
+//set up ejs as engine for rendering html
+app.set('view engine', 'ejs');
+
 app.get('/', (req, res) => {
-    res.send('Hello from Node and Express !!');
+    res.render('index');
 });
 
 app.listen(PORT, ()=>{console.log(`listening on port: ${PORT}`);});
