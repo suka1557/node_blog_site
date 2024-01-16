de93a06c18a3bce81b4f2097127d4e84557c9393
sukant.kumar <sukant.kumar@ril.com>
2024-01-16 10:25:44 +0530

    Passing a variable from App.js to html template and rendering it on page

--- App.js
+++ App.js
@@ -5,8 +5,11 @@ const PORT = 5024;
 //set up ejs as engine for rendering html
 app.set('view engine', 'ejs');
 
+//Checking passing of variables from JS file to EJS template to be render
+const developer_name = "Sukant Kumar";
+
 app.get('/', (req, res) => {
-    res.render('index');
+    res.render('index', {developer_name});
 });
 
 app.listen(PORT, ()=>{console.log(`listening on port: ${PORT}`);});
