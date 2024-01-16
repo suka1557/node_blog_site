dff257d4589b8f04972bcff8a6d756ec4c41d087
sukant.kumar <sukant.kumar@ril.com>
2024-01-16 09:54:39 +0530

    Created App.js files with basic route sending back a text

---
+++ App.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+const PORT = 5024;
+
+app.get('/', (req, res) => {
+    res.send('Hello from Node and Express - Small Change');
+});
+
+app.listen(PORT, ()=>{console.log(`listening on port: ${PORT}`);});
