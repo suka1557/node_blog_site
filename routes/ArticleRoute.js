14a4166b590146fdc8ae15361debb77b5040fe3b
sukant.kumar <sukant.kumar@ril.com>
2024-01-22 12:23:19 +0530

    Changed code to add author, store form data in mongo collection and redirect to homepage

--- routes/ArticleRoute.js
+++ routes/ArticleRoute.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const article_route = express.Router();
+const mongo_collection = require('../connections/MongoConnect');
 
 // Middleware to parse form data
 article_route.use(express.urlencoded({ extended: true }));
@@ -8,11 +9,42 @@ article_route.get('/new',(req, res)=>{
     res.render('new_article_form');
 });
 
-article_route.post('/create_new_article',(req, res)=>{
-    const {ArticleTitle, ArticleDescription, ArticleContent} = req.body;
+article_route.post('/create_new_article', async (req, res)=>{
+    const {article_title, author_name, article_description, article_content} = req.body;
+    
+    // Get the current date and time
+    const currentDate = new Date();
+
+    // Get the Unix time (milliseconds since the Unix epoch)
+    const unixTimeMilliseconds = currentDate.getTime();
+
+    // Convert milliseconds to seconds (Unix time format)
+    const unixTimeSeconds = Math.floor(unixTimeMilliseconds / 1000);
+    const created_at_unix_timestamp = unixTimeSeconds;
+    const updated_at_unix_timestamp = unixTimeSeconds;
+
+    const newRecord = {
+        'article_title': article_title,
+        'author_name': author_name,
+        'article_description':article_description,
+        'article_content':article_content,
+        'created_at_unix_timestamp':created_at_unix_timestamp,
+        'updated_at_unix_timestamp':updated_at_unix_timestamp,
+    };
+
+    await mongo_collection.insertOne(newRecord);
+
+    // Use client-side script to display a popup message
+    const popupScript = `
+      <script>
+        alert('Article Submitted Successfully');
+        window.location.href = '/';
+      </script>
+    `;
 
 
-    res.send(`Form submitted successfully! - ${ArticleTitle} - ${ArticleDescription} - ${ArticleContent}`);
+    res.send(popupScript);
+
 });
 
 module.exports = article_route;
