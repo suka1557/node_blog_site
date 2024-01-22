10acf8f42523505fd143402b79aad09af175ecc0
sukant.kumar <sukant.kumar@ril.com>
2024-01-22 07:10:52 +0530

    Connected Form submission to endpoint in article route and getting access to fields via name property in the HTML form

--- routes/ArticleRoute.js
+++ routes/ArticleRoute.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const article_route = express.Router();
 
+// Middleware to parse form data
+article_route.use(express.urlencoded({ extended: true }));
+
 article_route.get('/new',(req, res)=>{
     res.render('new_article_form');
+});
+
+article_route.post('/create_new_article',(req, res)=>{
+    const {ArticleTitle, ArticleDescription, ArticleContent} = req.body;
+
+
+    res.send(`Form submitted successfully! - ${ArticleTitle} - ${ArticleDescription} - ${ArticleContent}`);
 });
 
 module.exports = article_route;
