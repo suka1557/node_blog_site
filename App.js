217e3363c88dad67d50f9ef2f87e4235cb423d68
sukant.kumar <sukant.kumar@ril.com>
2024-01-22 20:07:57 +0530

    Changed homepage to read data from database and list the articles

--- App.js
+++ App.js
@@ -2,8 +2,8 @@ const express = require('express');
 const app = express();
 const PORT = 5024;
 const ArticleRoute = require('./routes/ArticleRoute');
+const mongo_collection = require('./connections/MongoConnect');
 
-const article_list = require('./data/ArticleList');
 
 //set up ejs as engine for rendering html
 app.set('view engine', 'ejs');
@@ -14,7 +14,29 @@ app.use('/articles', ArticleRoute);
 //Checking passing of variables from JS file to EJS template to be render
 const developer_name = "Sukant Kumar";
 
-app.get('/', (req, res) => {
+app.get('/', async (req, res) => {
+
+    // Assuming you want to find all documents in the collection
+    const documents = await mongo_collection.find({}).toArray();
+    // Sort the array in descending order based on updated_at
+    documents.sort((a, b) => b.updated_at_unix_timestamp - a.updated_at_unix_timestamp);
+
+    // Now, `documents` is an array containing the retrieved data
+    const article_list = documents.map((record) => {
+            // Process each document if needed
+            const dateOptions = { day: '2-digit', month: 'short', year: 'numeric' };
+            const formattedDate = new Date(record.updated_at_unix_timestamp * 1000).toLocaleDateString('en-US', dateOptions);
+
+            
+                return {
+                    title : record.article_title,
+                    author: record.author_name,
+                    description : record.article_description,
+                    formatted_date: formattedDate,
+                };
+
+        });
+
     res.render('index', {articles: article_list, developer_name});
 });
 
